b52dd8a4a25419ee846e0092312ca8f259ab941a
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-04 09:36:23 +0100

    use half-res boot animation
    Change-Id: I0f567318aeea351b1c1e1f6bb75b8588e3e9a91e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,6 +108,9 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/
 # Include an expanded selection of fonts
 EXTENDED_FONT_FOOTPRINT := true
 
+# Boot animation
+TARGET_BOOTANIMATION_HALF_RES := true
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
