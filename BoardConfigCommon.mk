49f8a0991d97bcb7ed6e89ef16590c399c89e469
Joe Wooster <superhanss@aospal.com>
2015-06-30 14:52:34 +0100

    fusion3: healthd: Set LED paths
    Change-Id: I2c55b3ade6dc228e14783dac27824e5bef1b4bda

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,9 @@ HAVE_ADRENO_SOURCE := false
 
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
+RED_LED_PATH := /sys/class/leds/lm3533-red/brightness
+GREEN_LED_PATH := /sys/class/leds/lm3533-green/brightness
+BLUE_LED_PATH := /sys/class/leds/lm3533-blue/brightness
 
 # Sensors
 SOMC_CFG_SENSORS := true
